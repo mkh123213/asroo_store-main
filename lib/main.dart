0c34fcb52e0c5d1a9fab834a68a5595b15407223
DESKTOP-EPEH8I3\IDR-46 <mohammadwork199700@gmail.com>
2026-04-22 14:18:51 +0300

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-// test
+// test3
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
